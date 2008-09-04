2bf8189a5a21058931696c36c64987e8e9d632cf
Jeffrey Chu <jochu@uci.edu>
2008-09-03 21:06:05 -0700

    fix some indentation on swank.clj

--- swank/swank.clj
+++ swank/swank.clj
@@ -1,12 +1,12 @@
 (clojure/create-ns 'swank)
 (clojure/ns swank
-    (:import (java.io InputStreamReader PushbackReader StringReader Reader
-                       BufferedReader FileReader
-                       OutputStreamWriter FileWriter Writer StringWriter
-                       OutputStream PrintStream File)
-             (clojure.lang LineNumberingPushbackReader)
-             (java.net ServerSocket Socket InetAddress)
-             (java.util.zip ZipFile)))
+  (:import (java.io InputStreamReader PushbackReader StringReader Reader
+                    BufferedReader FileReader
+                    OutputStreamWriter FileWriter Writer StringWriter
+                    OutputStream PrintStream File)
+           (clojure.lang LineNumberingPushbackReader)
+           (java.net ServerSocket Socket InetAddress)
+           (java.util.zip ZipFile)))
 (clojure/refer 'clojure :exclude '(load-file))
 
 (load-resources "swank-clojure.clj" "swank-c-p-c.clj" "swank-arglists.clj")
