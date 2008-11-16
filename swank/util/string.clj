ef58eb4953f23746ade2c142c8d6a98d0f03f314
Jeffrey Chu <jochu0@gmail.com>
2008-11-16 14:12:47 -0800

    start a string util

---
+++ swank/util/string.clj
@@ -0,0 +1,7 @@
+(ns swank.util.string)
+
+(defn largest-common-prefix
+  "Returns the largest common prefix of two strings."
+  ([#^String a, #^String b]
+     (apply str (take-while (comp not nil?) (map #(when (= %1 %2) %1) a b))))
+  {:tag String})
