2345519602c101b1c795d30227aa460bea3af378
Kiprianas Spiridonovas <neegzistuoju@gmail.com>
2008-11-16 15:11:52 +0200

    Clean up compound completion code and implement acronym completion. (cc TAB galc TAB => clojure.core/gen-and-load-class)

--- swank/commands/contrib/swank_c_p_c.clj
+++ swank/commands/contrib/swank_c_p_c.clj
@@ -1,6 +1,13 @@
 (ns swank.commands.contrib.swank-c-p-c
   (:use (swank util core commands)))
 
+(defn- unacronym
+  "Interposes delimiter between each character of string."
+  ([delimiter,
+    #^String string]
+     (apply str (interpose delimiter string)))
+  {:tag String})
+
 ;;; Ported from slime's swank.
 (defn- compound-prefix-match
   "Takes a `prefix' and a `target' string and which returns position
@@ -13,24 +20,31 @@
   compound-prefix of `target'."
   [delimeter,
    #^String prefix,
-   #^String target]
+   #^String target,
+   & [no-acronyms?]]
   (if (= "" prefix)
     0
-    (loop [prefix prefix
-           tpos 0]
-      (let [ch (first prefix)
-            new-tpos (if (= ch delimeter)
-                       (position delimeter target tpos)
-                       tpos)]
-        (when (and tpos
-                   (< tpos (.length target))
-                   (if (not= tpos new-tpos)
-                     new-tpos
-                     (= ch (.charAt target tpos))))
-          (if-let [newprefix (rest prefix)]
-              (recur newprefix
-                     (inc new-tpos))
-            new-tpos))))))
+    (if (not no-acronyms?)
+      (or (compound-prefix-match delimeter prefix target true)
+          (compound-prefix-match delimeter
+                                 (unacronym delimeter prefix)
+                                 target
+                                 true))
+      (loop [prefix prefix
+             tpos 0]
+        (let [ch (first prefix)
+              new-tpos (if (= ch delimeter)
+                         (position delimeter target tpos)
+                         tpos)]
+          (when (and tpos
+                     (< tpos (.length target))
+                     (if (not= tpos new-tpos)
+                       new-tpos
+                       (= ch (.charAt target tpos))))
+            (if-let [newprefix (rest prefix)]
+                (recur newprefix
+                       (inc new-tpos))
+              new-tpos)))))))
 
 (defn- symbol-name-parts
   "Parses a symbol name into a namespace and a name. If name doesn't
@@ -39,11 +53,10 @@
   (let [ns-pos (.indexOf symbol (int \/))]
     (cond
      (= ns-pos -1) ;; no slash?
-       [nil symbol nil]
+     [nil symbol]
      :else
-       [(.substring symbol 0 ns-pos)
-        (.substring symbol (inc ns-pos))
-        ns-pos])))
+     [(.substring symbol 0 ns-pos)
+      (.substring symbol (inc ns-pos))])))
 
 (defn- ns-exists
   "Given an string its-name, returns either an ns if a like named ns
@@ -54,35 +67,41 @@
 
 (defn- completion-list
   "Returns a list of vars or nses (depending on value of of-what:
-  either :var or :ns) that are possible completions of sym, given that
-  maybe-ns is nil or an ns in which to search of vars if of-what
-  is :var, and current-ns is the ns of the context of the completion
-  or nil."
+  either :var or :ns) that are possible compound completions of sym,
+  given that maybe-ns is nil or an ns in which to search of vars if
+  of-what is :var, and current-ns is the ns of the context of the
+  completion or nil.
+
+  The compound completion delimeter is `.' for namespaces and `-' for
+  symbols."
   [of-what, #^String sym, & [maybe-ns, current-ns]]
-  (let [no-ns? (not maybe-ns)
-        maybe-ns (or maybe-ns current-ns)]
-    (cond
-     (= :ns of-what)
-       (filter (partial compound-prefix-match \. sym)
-               (map (comp name ns-name)
-                    (all-ns)))
-     (and maybe-ns
-          (= :var of-what))
-       (map (if no-ns?
-              identity
-              (partial str maybe-ns \/))
-            (filter (partial compound-prefix-match \- sym)
-                    (map (comp name :name meta)
-                         (filter var? (vals ((if (= maybe-ns current-ns)
-                                               ns-map
-                                               ns-publics)
-                                             maybe-ns)))))))))
+  (cond
+   (= :ns of-what)
+   (filter (partial compound-prefix-match \. sym)
+           (map (comp name ns-name)     ;name of ns as String
+                (all-ns)))
+   (= :var of-what)
+   (map
+    (if maybe-ns
+      (partial str maybe-ns \/)
+      identity)
+    (filter
+     (partial compound-prefix-match \- sym)
+     (map
+      (comp name :name meta)            ;name of var as String
+      (filter var?
+              (vals (if (or (not maybe-ns)
+                            (= maybe-ns current-ns))
+                      ;; In current namespace, complete to all vars,
+                      ;; in other namespaces -- only to public vars.
+                      (ns-map current-ns)
+                      (ns-publics maybe-ns)))))))))
 
 (defn- compound-complete
   "Returns a list of possible completions of sym in cur-ns."
   [#^String sym,
    #^String cur-ns]
-  (let [[sym-ns sym-name sym-slash] (symbol-name-parts sym)
+  (let [[sym-ns sym-name] (symbol-name-parts sym)
         sym-ns (ns-exists sym-ns)
         cur-ns (ns-exists cur-ns)]
     (if sym-ns
@@ -94,17 +113,13 @@
 
 (defn- largest-common-prefix
   "Returns the largest common prefix of two strings."
-  ([#^String a #^String b]
+  ([#^String a, #^String b]
      (apply str (take-while (comp not nil?) (map #(when (= %1 %2) %1) a b))))
   {:tag String})
 
 (defslimefn completions [string package]
-  (try
-   (let [matches (sort (compound-complete string package))
-         longest-comp (if matches
-                        (reduce largest-common-prefix matches)
-                        string)]
-     (list matches longest-comp))
-   ;; comment the following sexp if debugging completion.
-   (catch Throwable e
-     (list nil string))))
+  (let [matches (sort (compound-complete string package))
+        longest-comp (if matches
+                       (reduce largest-common-prefix matches)
+                       string)]
+    (list matches longest-comp)))
