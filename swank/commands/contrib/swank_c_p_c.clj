636e31de9a61c5557b8eea25895b6aea3ec2fcc0
Jeffrey Chu <jochu0@gmail.com>
2008-11-18 08:31:37 -0800

    split completion-list into two functions, ns and var

--- swank/commands/contrib/swank_c_p_c.clj
+++ swank/commands/contrib/swank_c_p_c.clj
@@ -48,40 +48,34 @@
   (and its-name
        (find-ns (symbol its-name))))
 
-(defn- completion-list
-  "Returns a list of vars or nses (depending on value of of-what:
-  either :var or :ns) that are possible compound completions of sym,
-  given that maybe-ns is nil or an ns in which to search of vars if
-  of-what is :var, and current-ns is the ns of the context of the
-  completion or nil.
+(defn- completion-list-ns
+  "Returns a list of nses that are possible compound completions of sym.
+   The compound completion delimeter is `.'"
+  ([sym]
+     (filter (partial compound-prefix-match-acronyms \. sym)
+             (map (comp name ns-name) (all-ns)))))
 
-  The compound completion delimeter is `.' for namespaces and `-' for
-  symbols."
-  ([of-what sym]          (completion-list of-what sym nil))
-  ([of-what sym maybe-ns] (completion-list of-what sym maybe-ns nil))
-  ([of-what #^String sym maybe-ns current-ns]
-     (cond
-      (= :ns of-what)
-      (filter (partial compound-prefix-match-acronyms \. sym)
-              (map (comp name ns-name)  ;name of ns as String
-                   (all-ns)))
-      
-      (= :var of-what)
-      (map
-       (if maybe-ns
-         (partial str maybe-ns \/)
-         identity)
-       (filter
-        (partial compound-prefix-match-acronyms \- sym)
-        (map
-         (comp name :name meta)         ;name of var as String
-         (filter var?
-                 (vals (if (or (not maybe-ns)
-                               (= maybe-ns current-ns))
-                         ;; In current namespace, complete to all vars,
-                         ;; in other namespaces -- only to public vars.
-                         (ns-map current-ns)
-                         (ns-publics maybe-ns))))))))))
+(defn- completion-list-var
+  "Returns a list of vars that are possible compound completions of sym,
+   given that maybe-ns is nil or an ns in which to search of vars, and
+   current-ns is the ns of the context of the completion or nil.
+   The compound completion delimeter is `-'"
+  ([sym maybe-ns current-ns]
+     (map
+      (if maybe-ns
+        (partial str maybe-ns \/)
+        identity)
+      (filter
+       (partial compound-prefix-match-acronyms \- sym)
+       (map
+        (comp name :name meta)          ;name of var as String
+        (filter var?
+                (vals (if (or (not maybe-ns)
+                              (= maybe-ns current-ns))
+                        ;; In current namespace, complete to all vars,
+                        ;; in other namespaces -- only to public vars.
+                        (ns-map current-ns)
+                        (ns-publics maybe-ns)))))))))
 
 (defn- compound-complete
   "Returns a list of possible completions of sym in cur-ns."
@@ -91,11 +85,11 @@
         sym-ns (ns-exists? sym-ns)
         cur-ns (ns-exists? cur-ns)]
     (if sym-ns
-      (completion-list :var sym-name sym-ns cur-ns)
+      (completion-list-var sym-name sym-ns cur-ns)
       (concat
-       (completion-list :var sym-name nil cur-ns)
+       (completion-list-var sym-name nil cur-ns)
        (map #(str % \/)
-            (completion-list :ns sym-name))))))
+            (completion-list-ns sym-name))))))
 
 (defslimefn completions [string package]
   (let [matches (sort (compound-complete string package))
