4d8e9244447490a86727b91cd85c475e0b5701f5
Jeffrey Chu <jochu0@gmail.com>
2009-04-17 13:37:27 -0700

    suppress namespaces, make function instead of macro

--- swank/util/pprint.clj
+++ swank/util/pprint.clj
@@ -1,12 +1,21 @@
 (ns swank.util.pprint)
 
-(def pprint-enabled?
+
+(def
+ #^{:private true}
+ pprint-enabled?
      (try (.loadClass (clojure.lang.RT/baseLoader) "clojure.contrib.pprint.PrettyWriter") (catch Exception e nil)))
 
 (when pprint-enabled?
   (use 'clojure.contrib.pprint))
 
-(defmacro pretty-pr-code [code]
-  (if pprint-enabled?
-    `(with-pprint-dispatch *code-dispatch* (write ~code :pretty true :stream nil))
-    `(pr-str ~code)))
+(defmacro pretty-pr-code*
+  ([code]
+     (if pprint-enabled?
+       `(binding [*print-suppress-namespaces* true]
+          (with-pprint-dispatch *code-dispatch* (write ~code :pretty true :stream nil)))
+       `(pr-str ~code)))
+  {:private true})
+
+(defn pretty-pr-code [code]
+  (pretty-pr-code* code))
