b40efcd668079af6cf053380670d6b7dd2ba4ec6
Phil Hagelberg <technomancy@gmail.com>
2009-09-29 14:24:21 -0700

    Clean up io.clj

--- src/swank/util/io.clj
+++ src/swank/util/io.clj
@@ -1,10 +1,11 @@
 (ns swank.util.io
-  (:use (swank util)
-        (swank.util.concurrent thread)))
+  (:use [swank util]
+        [swank.util.concurrent thread])
+  (:import [java.io StringWriter Reader]))
 
 (defn read-chars
   ([rdr n] (read-chars rdr n false))
-  ([#^java.io.Reader rdr n throw-exception]
+  ([#^Reader rdr n throw-exception]
      (let [sb (StringBuilder.)]
        (dotimes [i n]
          (let [c (.read rdr)]
@@ -17,24 +18,21 @@
 (defn call-on-flush-stream
   "Creates a stream that will call a given function when flushed."
   ([flushf]
-     (let [closed? (ref nil)
-           
-           #^java.io.StringWriter
-           stream (proxy [java.io.StringWriter] []
-                    (close [] (dosync (ref-set closed? true)))
-                    (flush []
-                      (let [#^java.io.StringWriter me this] ;; only so it know what it is
-                        (let [len (.. me getBuffer length)]
-                          (when (> len 0)
-                            (flushf (.. me getBuffer (substring 0 len)))
-                            (.. me getBuffer (delete 0 len)))))))]
-
+     (let [closed? (atom false)
+           #^StringWriter stream
+           (proxy [StringWriter] []
+             (close [] (reset! closed? true))
+             (flush []
+                    (let [#^StringWriter me this
+                          len (.. me getBuffer length)]
+                      (when (> len 0)
+                        (flushf (.. me getBuffer (substring 0 len)))
+                        (.. me getBuffer (delete 0 len))))))]
        (dothread
-         (thread-set-name "Call-on-write Stream")
-         (continuously
-           (Thread/sleep 200)
-           (when-not @closed?
-             (.flush stream))))
-       
+        (thread-set-name "Call-on-write Stream")
+        (continuously
+         (Thread/sleep 200)
+         (when-not @closed?
+           (.flush stream))))
        stream))
-  {:tag java.io.StringWriter})
+  {:tag StringWriter})
