f119da6e641905f8725938c780be30ed39ebc56e
Phil Hagelberg <technomancy@gmail.com>
2009-11-20 10:53:23 -0800

    Don't squat the org.clojure groupId.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/swank-clojure "1.1.0-SNAPSHOT"
+(defproject swank-clojure "1.1.0-SNAPSHOT"
   :description "SWANK server connecting Clojure to Emacs SLIME"
   :url "http://github.com/technomancy/swank-clojure"
   :main swank.swank
