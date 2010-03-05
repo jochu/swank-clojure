2b35c80d6bf70ddfe3f9c2f238b94108df8af17a
Hugo Duncan <hugo_duncan@yahoo.com>
2010-03-04 21:12:34 -0500

    changed package detection regexp to take meta-data declaration into consideration.  Still has issues when the metadata contains braces or escaped quotes.

--- swank-clojure.el
+++ swank-clojure.el
@@ -159,11 +159,11 @@ this, keep that in mind."
            (format "%s" (slime-coding-system-cl-name encoding)))))
 
 (defun swank-clojure-find-package ()
-  (let ((regexp "^(\\(clojure.core/\\)?\\(in-\\)?ns\\s-+[:']?\\([^()\" \t\n]+\\>\\)"))
+  (let ((regexp "^(\\(clojure.core/\\)?\\(in-\\)?ns[ \t\n\r]+\\(#\\^{[^}]+}[ \t\n\r]+\\)?[:']?\\([^()\" \t\n]+\\>\\)"))
     (save-excursion
       (when (or (re-search-backward regexp nil t)
                 (re-search-forward regexp nil t))
-        (match-string-no-properties 3)))))
+        (match-string-no-properties 4)))))
 
 ;;;###autoload
 (defun swank-clojure-slime-mode-hook ()
