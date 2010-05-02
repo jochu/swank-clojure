87989e08fbc6fe642bf915596f8dcd6cc6ef318a
Phil Hagelberg <technomancy@gmail.com>
2010-05-02 15:31:45 -0700

    Bump Clojure dev dependency to 1.2.0-master-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject swank-clojure "1.2.0-SNAPSHOT"
   :description "Swank server connecting Clojure to Emacs SLIME"
   :url "http://github.com/technomancy/swank-clojure"
-  :dev-dependencies [[org.clojure/clojure "1.1.0"]])
+  :dev-dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]])
