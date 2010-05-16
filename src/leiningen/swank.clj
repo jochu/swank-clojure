585cbdf9f7e34600d36b0667a4f5158195332d2e
Phil Hagelberg <technomancy@gmail.com>
2010-05-15 21:16:37 -0700

    Clean up lein-swank plugin.

--- src/leiningen/swank.clj
+++ src/leiningen/swank.clj
@@ -1,17 +1,14 @@
-(ns leiningen.swank)
+(ns leiningen.swank
+  (:use [leiningen.compile :only [eval-in-project]]))
 
-(try
-  (use '[leiningen.compile :only [eval-in-project]])
-  (defn swank
-    "Launch swank server for Emacs to connect. Optionally takes PORT and HOST."
-    ([project port host & opts]
-       (eval-in-project project
-                        `(do (require '~'swank.swank)
-                             (@(ns-resolve '~'swank.swank '~'start-repl)
-                              (Integer. ~port)
-                              ~@(concat (map read-string opts)
-                                        [:host host])))))
-    ([project port] (swank project port "localhost"))
-    ([project] (swank project 4005)))
-  (catch Exception _
-    (println "This namespace is meant to be used from Leiningen.")))
+(defn swank
+  "Launch swank server for Emacs to connect. Optionally takes PORT and HOST."
+  ([project port host & opts]
+     (eval-in-project project
+                      `(do (require '~'swank.swank)
+                           (@(ns-resolve '~'swank.swank '~'start-repl)
+                            (Integer. ~port)
+                            ~@(concat (map read-string opts)
+                                      [:host host])))))
+  ([project port] (swank project port "localhost"))
+  ([project] (swank project 4005)))
