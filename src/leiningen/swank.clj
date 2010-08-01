eb1d0635ec87782f9d972b463734ed98b139b111
Alex Ott <alexott@gmail.com>
2010-08-01 17:29:17 +0200

    implement loading of global init script on 'lein swank' invocation

--- src/leiningen/swank.clj
+++ src/leiningen/swank.clj
@@ -1,5 +1,6 @@
 (ns leiningen.swank
-  (:use [leiningen.compile :only [eval-in-project]])
+  (:use [leiningen.compile :only [eval-in-project]]
+        [leiningen.core :only [get-global-init-script]])
   (:import [java.io File]))
 
 (defn swank
@@ -7,13 +8,16 @@
   ([project port host & opts]
      (eval-in-project project
                       `(do
-                         (let [is# ~(:init-script project)]
-                           (when (and is# (.exists (File. is#)))
+                         (let [is# ~(:init-script project)
+                               gis# ~(get-global-init-script)]
+                           (when (not (nil? gis#))
+                             (load-file gis#))
+                           (when (and (not (nil? is#)) (.exists (File. (str is#))))
                              (load-file is#)))
                          (require '~'swank.swank)
-                           (@(ns-resolve '~'swank.swank '~'start-repl)
-                            (Integer. ~port)
-                            ~@(concat (map read-string opts)
-                                      [:host host])))))
+                         (@(ns-resolve '~'swank.swank '~'start-repl)
+                          (Integer. ~port)
+                          ~@(concat (map read-string opts)
+                                    [:host host])))))
   ([project port] (swank project port "localhost"))
   ([project] (swank project 4005)))
