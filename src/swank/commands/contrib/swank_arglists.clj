813ff517a787b348e2b3b2299a7deff5c3e7b505
Scott Jaderholm <jaderholm@gmail.com>
2010-09-16 07:07:05 -0400

    Added function name and highlighting of current arg to autodoc.

--- src/swank/commands/contrib/swank_arglists.clj
+++ src/swank/commands/contrib/swank_arglists.clj
@@ -3,6 +3,43 @@
 
 ((slime-fn 'swank-require) :swank-c-p-c)
 
+(defn position-in-params? [params pos]
+  (or (some #(= '& %) params)
+      (<= pos (count params))))
+
+;; (position-in-params? '[x y] 2)
+
+(defn highlight-position [params pos]
+  (if (<= pos (count (take-while #(not= % '&) params)))
+    (into [] (concat (take (dec pos) params)
+                     '(===>)
+                     (list (nth params (dec pos)))
+                     '(<===)
+                     (drop pos params)))
+    (if (some #(= % '&) params)
+      (into [] (concat (take-while #(not= % '&) params)
+                       '(===>)
+                       '(&)
+                       (list (last params))
+                       '(<===))))))
+
+;; (highlight-position '[x y] 1)
+
+(defn highlight-param-lists [params-list pos]
+  (loop [checked []
+         current (first params-list)
+         remaining (rest params-list)]
+    (if (position-in-params? current pos)
+      (concat checked
+              [(highlight-position current pos)]
+              remaining)
+      (when (seq remaining)
+        (recur (conj checked current)
+               (first remaining)
+               (rest remaining))))))
+
+;; (highlight-param-lists '([fname & body]) 1)
+
 (defslimefn arglist-for-echo-area [raw-specs & options]
   (let [{:keys [arg-indices
                 print-right-margin
@@ -11,7 +48,13 @@
     (if (and raw-specs
              (seq? raw-specs)
              (seq? (first raw-specs)))
-      ((slime-fn 'operator-arglist) (ffirst raw-specs) *current-package*)
+      (let [result ((slime-fn 'operator-arglist) (ffirst raw-specs) *current-package*)
+            pos (first (second options))
+            cmd (ffirst raw-specs)]
+        (str cmd ": "
+             (if (or (zero? pos) (nil? result))
+               nil
+               (apply list (highlight-param-lists (read-string result) pos)))))
       nil)))
 
 (defslimefn variable-desc-for-echo-area [variable-name]
