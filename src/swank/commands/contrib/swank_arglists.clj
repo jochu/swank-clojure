d3a8c7f0ac7df3442f53fefb413913a0ef88bcfd
Scott Jaderholm <jaderholm@gmail.com>
2010-09-17 14:32:26 +0800

    Rewrote code, much clearer! Made several improvements: in apply form, if not on apply name shows arglist for function being applied, and in first level of binding forms highlights binding correctly.

--- src/swank/commands/contrib/swank_arglists.clj
+++ src/swank/commands/contrib/swank_arglists.clj
@@ -3,59 +3,99 @@
 
 ((slime-fn 'swank-require) :swank-c-p-c)
 
-(defn position-in-params? [params pos]
-  (or (some #(= '& %) params)
-      (<= pos (count params))))
+;;; pos starts at 1 bc 0 is function name
+(defn position-in-arglist? [arglist pos]
+  (or (some #(= '& %) arglist)
+      (<= pos (count arglist))))
 
-;; (position-in-params? '[x y] 2)
+;; (position-in-arglist? '[x y] 2)
+;; => true
 
-(defn highlight-position [params pos]
-  (if (<= pos (count (take-while #(not= % '&) params)))
-    (into [] (concat (take (dec pos) params)
-                     '(===>)
-                     (list (nth params (dec pos)))
-                     '(<===)
-                     (drop pos params)))
-    (if (some #(= % '&) params)
-      (into [] (concat (take-while #(not= % '&) params)
-                       '(===>)
-                       '(&)
-                       (list (last params))
-                       '(<===))))))
+(defn highlight-position [arglist pos]
+  (if (zero? pos)
+    arglist
+    ;; i.e. not rest args
+    (let [num-normal-args (count (take-while #(not= % '&) arglist))]
+      (if (<= pos num-normal-args)
+        (into [] (concat (take (dec pos) arglist)
+                         '(===>)
+                         (list (nth arglist (dec pos)))
+                         '(<===)
+                         (drop pos arglist)))
+        (let [rest-arg? (some #(= % '&) arglist)]
+          (if rest-arg?
+            (into [] (concat (take-while #(not= % '&) arglist)
+                             '(===>)
+                             '(&)
+                             (list (last arglist))
+                             '(<===)))))))))
 
-;; (highlight-position '[x y] 1)
+;; (highlight-position '[x y] 0)
+;; => [===> x <=== y]
 
-(defn highlight-param-lists [params-list pos]
-  (loop [checked []
-         current (first params-list)
-         remaining (rest params-list)]
-    (if (position-in-params? current pos)
-      (concat checked
-              [(highlight-position current pos)]
-              remaining)
-      (when (seq remaining)
-        (recur (conj checked current)
-               (first remaining)
-               (rest remaining))))))
+(defn highlight-arglists [arglists pos]
+  (let [arglists (read-string arglists)]
+    (loop [checked []
+           current (first arglists)
+           remaining (rest arglists)]
+      (if (position-in-arglist? current pos)
+        (into '() (concat checked
+                          [(highlight-position current pos)]
+                          remaining))
+        (when (seq remaining)
+          (recur (conj checked current)
+                 (first remaining)
+                 (rest remaining)))))))
 
-;; (highlight-param-lists '([fname & body]) 1)
+;; (highlight-arglists "([x] [x & more])" 1)
+;; => ([===> x <===] [x & more])
+
+(defmacro dbg[x] `(let [x# ~x] (println '~x "->" x#) x#))
+
+(defn arglists-for-fname [fname]
+  ((slime-fn 'operator-arglist) fname *current-package*))
+
+(defn message-format [cmd arglists pos]
+  (str (when cmd (str cmd ": "))
+       (when arglists
+         (if pos
+           (highlight-arglists arglists pos)
+           arglists))))
+
+(defn handle-apply [raw-specs pos]
+  (let [fname (second (first raw-specs))]
+    (message-format fname (arglists-for-fname fname) (dec pos))))
 
 (defslimefn arglist-for-echo-area [raw-specs & options]
   (let [{:keys [arg-indices
                 print-right-margin
                 print-lines]} (apply hash-map options)]
-    ;; Yeah, I'm lazy -- I'll flesh this out later
-    (if (and raw-specs
-             (seq? raw-specs)
-             (seq? (first raw-specs)))
-      (let [result ((slime-fn 'operator-arglist) (ffirst raw-specs) *current-package*)
-            pos (first (second options))
-            cmd (ffirst raw-specs)]
-        (str cmd ": "
-             (if (or (zero? pos) (nil? result))
-               nil
-               (apply list (highlight-param-lists (read-string result) pos)))))
-      nil)))
+    (if-not (and raw-specs
+                 (seq? raw-specs)
+                 (seq? (first raw-specs)))
+      nil ;; problem?
+      (let [pos (first (second options))
+            top-level? (= 1 (count raw-specs))
+            parent-pos (when-not top-level?
+                         (second (second options)))
+            fname (ffirst raw-specs)
+            parent-fname (when-not top-level?
+                           (first (second raw-specs)))
+            arglists (arglists-for-fname fname)
+            inside-binding? (and (not top-level?)
+                                 (#{"let" "binding" "doseq" "for" "loop"}
+                                  parent-fname)
+                                 (= 1 parent-pos))]
+        (dbg raw-specs)
+        (dbg options)
+        (cond
+         ;; display arglists for function being applied unless on top of apply
+         (and (= fname "apply") (not= pos 0)) (handle-apply raw-specs pos)              
+         ;; highlight binding inside binding forms unless >1 level deep
+         inside-binding? (message-format parent-fname
+                                         (arglists-for-fname parent-fname)
+                                         1)
+         :else  (message-format fname arglists pos))))))
 
 (defslimefn variable-desc-for-echo-area [variable-name]
   (with-emacs-package
