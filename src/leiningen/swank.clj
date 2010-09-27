89ef08d34c93ad4987ba3d98a68455f9d5c8737e
Phil Hagelberg <technomancy@gmail.com>
2010-09-27 13:22:14 -0700

    Typo.

--- src/leiningen/swank.clj
+++ src/leiningen/swank.clj
@@ -11,6 +11,6 @@
 (defn swank
   "Launch swank server for Emacs to connect. Optionally takes PORT and HOST."
   ([project port host & opts]
-     (eval-in-project project (swank-form post host opts)))
+     (eval-in-project project (swank-form port host opts)))
   ([project port] (swank project port "localhost"))
   ([project] (swank project 4005)))
