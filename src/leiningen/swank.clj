23a5d50d4a3ba93576d6066ba6585cf82bbde128
Phil Hagelberg <technomancy@gmail.com>
2010-09-27 11:06:49 -0700

    Factor out swank-form function for improved hookability.

--- src/leiningen/swank.clj
+++ src/leiningen/swank.clj
@@ -1,14 +1,16 @@
 (ns leiningen.swank
   (:use [leiningen.compile :only [eval-in-project]]))
 
+(defn swank-form [port host opts]
+  `(do (require '~'swank.swank)
+       (@(ns-resolve '~'swank.swank '~'start-repl)
+        (Integer. ~port)
+        ~@(concat (map read-string opts)
+                  [:host host]))))
+
 (defn swank
   "Launch swank server for Emacs to connect. Optionally takes PORT and HOST."
   ([project port host & opts]
-     (eval-in-project project
-                      `(do (require '~'swank.swank)
-                           (@(ns-resolve '~'swank.swank '~'start-repl)
-                            (Integer. ~port)
-                            ~@(concat (map read-string opts)
-                                      [:host host])))))
+     (eval-in-project project (swank-form post host opts)))
   ([project port] (swank project port "localhost"))
   ([project] (swank project 4005)))
