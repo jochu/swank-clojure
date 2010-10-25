eb8e177d7f6e20ae8688f84acf9cd662d5f8260a
Phil Hagelberg <technomancy@gmail.com>
2010-10-25 09:18:57 -0700

    Check for Clojure 1.2+ before falling back to contrib's pprint.

--- src/swank/clj_contrib/pprint.clj
+++ src/swank/clj_contrib/pprint.clj
@@ -1,27 +1,27 @@
 (ns swank.clj-contrib.pprint)
 
 (def #^{:private true} pprint-enabled?
-  (try ;; 1.0, 1.1
-    (.loadClass (clojure.lang.RT/baseLoader)
-                "clojure.contrib.pprint.PrettyWriter")
-    (require '[clojure.contrib.pprint :as pp])
+  (try ;; 1.2+
+    (.getResource (clojure.lang.RT/baseLoader) "clojure/pprint")
+    (require '[clojure.pprint :as pp])
     (defmacro #^{:private true} pretty-pr-code*
       ([code]
          (if pprint-enabled?
            `(binding [pp/*print-suppress-namespaces* true]
-              (pp/with-pprint-dispatch pp/*code-dispatch*
+              (pp/with-pprint-dispatch pp/code-dispatch
                 (pp/write ~code :pretty true :stream nil)))
            `(pr-str ~code))))
     true
     (catch Exception e
-      (try ;; 1.2
-        (.getResource (clojure.lang.RT/baseLoader) "clojure/pprint")
-        (require '[clojure.pprint :as pp])
+      (try ;; 1.0, 1.1
+        (.loadClass (clojure.lang.RT/baseLoader)
+                    "clojure.contrib.pprint.PrettyWriter")
+        (require '[clojure.contrib.pprint :as pp])
         (defmacro #^{:private true} pretty-pr-code*
           ([code]
              (if pprint-enabled?
                `(binding [pp/*print-suppress-namespaces* true]
-                  (pp/with-pprint-dispatch pp/code-dispatch
+                  (pp/with-pprint-dispatch pp/*code-dispatch*
                     (pp/write ~code :pretty true :stream nil)))
                `(pr-str ~code))))
         true
