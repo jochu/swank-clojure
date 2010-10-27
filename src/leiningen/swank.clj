4d8c90c151c46c4fbe711ca43b9213906f0e8101
Phil Hagelberg <technomancy@gmail.com>
2010-10-26 19:41:39 -0700

    Oops; that wasn't right. Thanks tomoj.

--- src/leiningen/swank.clj
+++ src/leiningen/swank.clj
@@ -2,10 +2,10 @@
   (:use [leiningen.compile :only [eval-in-project]])
   (:import [java.io File]))
 
-(defn swank-form [port host opts]
+(defn swank-form [project port host opts]
   `(do
      (let [is# ~(:repl-init-script project)]
-       (when (and is# (.exists (File. is#)))
+       (when (.exists (File. (str is#)))
          (load-file is#)))
      (require '~'swank.swank)
      (@(ns-resolve '~'swank.swank '~'start-repl)
@@ -16,6 +16,6 @@
 (defn swank
   "Launch swank server for Emacs to connect. Optionally takes PORT and HOST."
   ([project port host & opts]
-     (eval-in-project project (swank-form port host opts)))
+     (eval-in-project project (swank-form project port host opts)))
   ([project port] (swank project port "localhost"))
   ([project] (swank project 4005)))
